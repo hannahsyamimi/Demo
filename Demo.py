e9d21527e5cbedd0fb14b4053cd1d83d8bd82803
hannahsyamimi <109711276+hannahsyamimi@users.noreply.github.com>
2022-07-21 17:13:43 +0800

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -39,24 +39,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-iris_name = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-    features = pd.DataFrame(data, index=[0])
-    return features
-st.write(iris_name)
-
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input_features()
+st.dataframe(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
 
 st.subheader('Prediction')
 st.write(prediction)
